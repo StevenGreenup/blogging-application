d6f906d4b3fdce76b2ef806bead7b87d4ab7840f
StevenGreenup <greenup.steven@gmail.com>
2016-06-22 21:45:25 -0500

    fixed routes for the root directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts' => 'posts#index', as: :posts
+  get '/' => 'posts#index', as: :posts
   get 'posts/new' => 'posts#new', as: :new_post
   get 'posts/:id' => 'posts#show', as: :post
   get 'posts/:id/edit' => 'posts#edit', as: :edit_post
